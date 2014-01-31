abd0a5dcbd082da47cf79a641379fe2c005de761
Jack Hayter <jackhayter1992@gmail.com>
2014-01-31 10:44:32 +0000

    basic example version

--- src/app/templates.js
+++ src/app/templates.js
@@ -3,5 +3,8 @@ angular.module('templates', ['app/app.view.html']);
 angular.module('app/app.view.html', []).run(['$templateCache', function($templateCache) {
 	'use strict';
 	$templateCache.put('app/app.view.html',
-		'<h1>{{message}}</h1>');
+		'<h1>Data Attributes</h1>\n' +
+		'\n' +
+		'<div class="image" data-score="100"></div>\n' +
+		'');
 }]);
