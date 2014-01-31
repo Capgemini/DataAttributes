3cf08de61f4c4d44b8e2fd3ed5646b6c9122fec2
Jack Hayter <jackhayter1992@gmail.com>
2014-01-31 14:11:48 +0000

    first main example

--- dist/app.js
+++ dist/app.js
@@ -27,6 +27,29 @@ angular.module('app/app.view.html', []).run(['$templateCache', function($templat
 	$templateCache.put('app/app.view.html',
 		'<h1>Data Attributes</h1>\n' +
 		'\n' +
-		'<div class="image" data-score="100"></div>\n' +
-		'');
+		'<!-- <div class="image" data-score="100"></div>\n' +
+		' -->\n' +
+		'\n' +
+		'<ul class="small-block-grid-1 medium-block-grid-2 large-block-grid-4">\n' +
+		'  <li>\n' +
+		'  	<div class="image" data-year="2014">\n' +
+		'  		<img src="assets/img/inside.jpg">\n' +
+		'  	</div>\n' +
+		'  </li>\n' +
+		'  <li>\n' +
+		'  	<div class="image" data-year="1991">\n' +
+		'  		<img src="assets/img/barton.jpg" alt="" data-year="1991">\n' +
+		'  	</div>\n' +
+		'  </li>\n' +
+		'  <li>\n' +
+		'  	<div class="image" data-year="1996">\n' +
+		'			<img src="assets/img/fargo.jpg" alt="" data-year="1996">\n' +
+		'		</div>\n' +
+		'  </li>\n' +
+		'  <li>\n' +
+		'  	<div class="image" data-year="2000">\n' +
+		'  		<img src="assets/img/brother.jpg" alt="" data-year="2000">\n' +
+		'  	</div>\n' +
+		'  </li>\n' +
+		'</ul>');
 }]);
