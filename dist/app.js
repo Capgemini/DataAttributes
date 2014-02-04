6ff45490c351fb7f039e95639b1b7e48eb715b16
Jack Hayter <jackhayter1992@gmail.com>
2014-02-04 09:46:42 +0000

    added sort function

--- dist/app.js
+++ dist/app.js
@@ -1,6 +1,6 @@
 'use strict';
 
-angular.module('app', ['templates', 'common', 'ngRoute'])
+angular.module('app', ['templates', 'common', 'ngRoute', 'movies'])
 	.config(['$routeProvider', '$locationProvider', function ($routeProvider, $locationProvider) {
 		$routeProvider
 			.when('/', {
@@ -20,7 +20,53 @@ angular.module('app', ['templates', 'common', 'ngRoute'])
 
 angular.module('common', ['dependencies']);;'use strict';
 
-angular.module('dependencies', []);;angular.module('templates', ['app/app.view.html']);
+angular.module('dependencies', []);;'use strict';
+
+/**
+ * @ngdoc function
+ * @name ng.module:movies
+ * @function
+ *
+ * @description
+ * [add a description]
+ *
+ */
+angular.module('movies', [])
+
+	/**
+	 * @ngdoc function
+	 * @name ng.controller:moviesCtrl
+	 * @function
+	 *
+	 * @description
+	 * [add a description]
+	 *
+	 */
+	.controller('MoviesCtrl', ['$scope', function ($scope) {
+
+		$scope.movies = [
+			{
+				year: '2014',
+				rating: '7.9',
+				poster: 'assets/img/inside.jpg'
+			},
+			{
+				year: '1991',
+				rating: '7.8',
+				poster: 'assets/img/barton.jpg'
+			},
+			{
+				year: '1996',
+				rating: '8.2',
+				poster: 'assets/img/fargo.jpg'
+			},
+			{
+				year: '2000',
+				rating: '7.7',
+				poster: 'assets/img/brother.jpg'
+			}
+		];
+	}]);;angular.module('templates', ['app/app.view.html', 'app/movies.view.html']);
 
 angular.module('app/app.view.html', []).run(['$templateCache', function($templateCache) {
 	'use strict';
@@ -35,29 +81,22 @@ angular.module('app/app.view.html', []).run(['$templateCache', function($templat
 		'\n' +
 		'	<div class="row">\n' +
 		'		<div class="small-12 columns">\n' +
-		'			<ul class="small-block-grid-1 medium-block-grid-2 large-block-grid-4">\n' +
-		'			  <li>\n' +
-		'			  	<div class="image" data-year="2014" data-rating="7.9">\n' +
-		'			  		<img src="assets/img/inside.jpg">\n' +
-		'			  	</div>\n' +
-		'			  </li>\n' +
-		'			  <li>\n' +
-		'			  	<div class="image" data-year="1991" data-rating="7.8">\n' +
-		'			  		<img src="assets/img/barton.jpg">\n' +
-		'			  	</div>\n' +
-		'			  </li>\n' +
-		'			  <li>\n' +
-		'			  	<div class="image" data-year="1996" data-rating="8.2">\n' +
-		'						<img src="assets/img/fargo.jpg">\n' +
-		'					</div>\n' +
-		'			  </li>\n' +
-		'			  <li>\n' +
-		'			  	<div class="image" data-year="2000" data-rating="7.7">\n' +
-		'			  		<img src="assets/img/brother.jpg">\n' +
-		'			  	</div>\n' +
-		'			  </li>\n' +
-		'			</ul>\n' +
+		'			<ng-include src="\'app/movies.view.html\'"></ng-include>\n' +
 		'		</div>	\n' +
 		'	</div>\n' +
 		'</div>	');
+}]);
+
+angular.module('app/movies.view.html', []).run(['$templateCache', function($templateCache) {
+	'use strict';
+	$templateCache.put('app/movies.view.html',
+		'<div ng-controller="MoviesCtrl">\n' +
+		'	<ul class="small-block-grid-1 medium-block-grid-2 large-block-grid-4">\n' +
+		'		<li ng-repeat="movie in movies | orderBy:\'year\'">\n' +
+		'			<div class="image" data-year="{{movie.year}}" data-rating="{{movie.rating}}">\n' +
+		'				<img src="{{movie.poster}}">\n' +
+		'			</div>\n' +
+		'		</li>\n' +
+		'	</ul>\n' +
+		'</div>');
 }]);
