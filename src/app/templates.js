e3f070e93a7c06c828f32c601fb44bb75803c921
Jack Hayter <jackhayter1992@gmail.com>
2014-02-04 09:50:58 +0000

    added sort function

--- src/app/templates.js
+++ src/app/templates.js
@@ -1,4 +1,4 @@
-angular.module('templates', ['app/app.view.html']);
+angular.module('templates', ['app/app.view.html', 'app/movies.view.html']);
 
 angular.module('app/app.view.html', []).run(['$templateCache', function($templateCache) {
 	'use strict';
@@ -13,29 +13,22 @@ angular.module('app/app.view.html', []).run(['$templateCache', function($templat
 		'\n' +
 		'	<div class="row">\n' +
 		'		<div class="small-12 columns">\n' +
-		'			<ul class="small-block-grid-1 medium-block-grid-2 large-block-grid-4">\n' +
-		'			  <li>\n' +
-		'			  	<div class="image" data-year="2014" data-rating="7.9">\n' +
-		'			  		<img src="assets/img/inside.jpg">\n' +
-		'			  	</div>\n' +
-		'			  </li>\n' +
-		'			  <li>\n' +
-		'			  	<div class="image" data-year="1991" data-rating="7.8">\n' +
-		'			  		<img src="assets/img/barton.jpg">\n' +
-		'			  	</div>\n' +
-		'			  </li>\n' +
-		'			  <li>\n' +
-		'			  	<div class="image" data-year="1996" data-rating="8.2">\n' +
-		'						<img src="assets/img/fargo.jpg">\n' +
-		'					</div>\n' +
-		'			  </li>\n' +
-		'			  <li>\n' +
-		'			  	<div class="image" data-year="2000" data-rating="7.7">\n' +
-		'			  		<img src="assets/img/brother.jpg">\n' +
-		'			  	</div>\n' +
-		'			  </li>\n' +
-		'			</ul>\n' +
+		'			<ng-include src="\'app/movies.view.html\'"></ng-include>\n' +
 		'		</div>	\n' +
 		'	</div>\n' +
 		'</div>	');
+}]);
+
+angular.module('app/movies.view.html', []).run(['$templateCache', function($templateCache) {
+	'use strict';
+	$templateCache.put('app/movies.view.html',
+		'<div ng-controller="MoviesCtrl">\n' +
+		'	<ul class="small-block-grid-1 medium-block-grid-2 large-block-grid-4">\n' +
+		'		<li ng-repeat="movie in movies | orderBy:\'year\'">\n' +
+		'			<div class="image" data-year="{{movie.year}}" data-rating="{{movie.rating}}">\n' +
+		'				<img src="{{movie.poster}}">\n' +
+		'			</div>\n' +
+		'		</li>\n' +
+		'	</ul>\n' +
+		'</div>');
 }]);
